5a0632a29f8f2ee3bdaff8138411e198cb121016
Amar0911 <guptaamar0911@gmail.com>
2024-12-26 12:56:50 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,18 @@
+
+// function a(){
+    // const root = document.getElementById('root');
+//     const heading = React.createElement('h1', null, 'Hllo');
+//     ReactDOM.render(heading,root)
+// }
+// a();
+
+
+
+/* */
+
+const root = document.getElementById('root');
+function Heading(){
+    return <h1>Helllo</h1>
+}
+
+ReactDOM.render(<Heading/>, root);
