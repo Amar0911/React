84e9ae69b008ece3c24fb644b4f7a80417f7e10c
Amar0911 <guptaamar0911@gmail.com>
2025-01-09 23:49:45 +0530

    update

--- movie/src/pages/SearchedMoviePage.jsx
+++ movie/src/pages/SearchedMoviePage.jsx
@@ -1,6 +1,36 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
+import { useLocation } from 'react-router-dom';
+import MovieCard from '../components/MovieCard';
+
+
+const API_KEY = 'c45a857c193f6302f2b5061c3b85e743'; 
+const BASE_URL = 'https://api.themoviedb.org/3';
+
+
+// language=en-US&page=1 => query parameters
+// cart/2/ => body parameters is 2
+
+/*
+
+useLocation is  a React Router hook that returns  the current location object.
+The location object contains information about the current URL and how the current route was reached.
+
+
+*/
+
+function useQuery() {
+  return new URLSearchParams(useLocation().search);
+}
+
 
 const SearchedMoviePage = () => {
+  
+  const [movies, setMovies] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const query = useQuery().get('query');
+
+
+
   return (
     <div>SearchedMoviePage</div>
   )
--- movie/src/pages/UpcomingPage.jsx
+++ movie/src/pages/UpcomingPage.jsx
@@ -1,9 +1,34 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
+import MovieCard from '../components/MovieCard';
+
+const API_KEY = 'c45a857c193f6302f2b5061c3b85e743'; 
+const BASE_URL = 'https://api.themoviedb.org/3';
 
 const UpcomingPage = () => {
+
+  const [movies, setMovies] = useState([]);
+
+  useEffect(() => {
+    const fetchUpcomingMovies = async () => {
+      const response = await fetch(`${BASE_URL}/movie/upcoming?api_key=${API_KEY}&Language=en-US&page=1`);
+      const data = await response.json();
+      setMovies(data.results);
+    }
+    fetchUpcomingMovies();
+    
+  }, [])
+  
+
   return (
-    <div>UpcomingPage</div>
-  )
+    <div>
+      <h2>Upcoming Movies</h2>
+      <div>
+        {movies.map((movie) => (
+          <MovieCard key={movie.id} movie={movie} />
+        ))}
+      </div>
+    </div>
+  );
 }
 
 export default UpcomingPage
