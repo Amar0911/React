c1045353a64a673dcd161a6e5a2215fb8e698fcf
Amar0911 <guptaamar0911@gmail.com>
2025-01-10 17:18:14 +0530

    update

--- movie/src/pages/SearchedMoviePage.jsx
+++ movie/src/pages/SearchedMoviePage.jsx
@@ -12,7 +12,7 @@ const BASE_URL = 'https://api.themoviedb.org/3';
 
 /*
 
-useLocation is  a React Router hook that returns  the current location object.
+useLocation is a React Router hook that returns  the current location object.
 The location object contains information about the current URL and how the current route was reached.
 
 
@@ -30,10 +30,41 @@ const SearchedMoviePage = () => {
   const query = useQuery().get('query');
 
 
+  useEffect(() => {
+    const fetchMovies = async () => {
+      if (query) {
+        setLoading(true);
+        try {
+          const response = await fetch(`${BASE_URL}/search/movie?api_key=${API_KEY}&language=en-US&query=${query}&page=1`);
+          const data = await response.json();
+          setMovies(data.results || []);
+          console.log(data.results);
+        } catch (error) {
+          console.error('Failed to fetch movies', error);
+        } finally{
+          setLoading(false);
+        }
+      }
+    };
+    fetchMovies();
+  
+  }, [query])
+  
 
   return (
-    <div>SearchedMoviePage</div>
-  )
-}
+    <div className='Searched-movies-page'>
+      <h2>Searched Results</h2>
+      {loading ? <p>Loading...</p> : movies.length > 0 ? (
+        <div className='movies-grid'>
+          {movies.map(movie => (
+            <MovieCard key={movie.id} movie={movie} />
+          ))}
+    </div>
+      ) : (
+        <p>No movies found "{query}"</p>
+      )}
+    </div>
+  );
+};
 
 export default SearchedMoviePage
